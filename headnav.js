85bf5eace3cfab204276e61242209be244fed49b
Keith Estrella <keithestrella@gmail.com>
2023-03-06 22:19:50 -0500

    Added React and fixed Grid/Flex to index
    Modified Index to make use of JavaScript React to reduce code on html and moved Header/Nav and Footer to separate JS file to call instead of hard coding for each html page

---
+++ headnav.js
@@ -0,0 +1,29 @@
+function Navigation(){
+    return (
+        <div id="header">
+            <header>
+                <nav className="nav">
+                    <h1>Keith's Personal Github Website</h1>
+                    <ul className="nav-items">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="school_projects.html">School Projects</a></li>
+                        <li><a href="personal_projects.html">Personal Projects</a></li>
+                        <li><a href="resume.html">Resume</a></li>
+                        <li><a href="Blog">Blog</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+        <footer className="footer">
+		    <p>Copyright 2023 &copy; Keith's Website</p>
+	    </footer>
+    )
+}
+
+ReactDOM.render(<Navigation />, document.getElementById("top-header-navigation"))
+ReactDOM.render(<Footer />, document.getElementById("footer-copy"))
---
+++ index.js
@@ -0,0 +1,29 @@
+function MainContent(){
+    return (
+        <section className="section">
+		    <h1>Welcome!</h1>
+		    <br />
+	    	<p>My name is Keith Estrella! And this is my little part of the web! I'm recently graduated from University and have been studying computer programming first through Machine Learning and recently I've picked up Web Development.
+	    	Don't mind me as I try and put what I've studied to use in the making of this website. Odds are I may make a mistake or two as I continue practicing my Programming Skills on here. :D</p>
+	    </section>
+    )
+}
+
+function SideContent(){
+    return (
+        <aside className="aside">
+		    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vero perferendis autem harum excepturi placeat asperiores eum? Dignissimos aspernatur suscipit dolore. Eos hic, autem quaerat harum labore fuga id numquam. </p>
+	    </aside>
+    )
+}
+
+function Page(){
+    return (
+        <div id="index">
+            <MainContent />
+            <SideContent />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
